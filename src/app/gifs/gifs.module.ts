f011e222db84ecb7d29a0a756242224da32c4c94
satudev <satudev83@gmail.com>
2021-07-03 11:11:41 +0200

    First Commit

---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GifsPageComponent } from './gifs-page/gifs-page.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+
+
+
+@NgModule({
+  declarations: [
+    GifsPageComponent,
+    BusquedaComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    GifsPageComponent
+  ]
+})
+export class GifsModule { }
