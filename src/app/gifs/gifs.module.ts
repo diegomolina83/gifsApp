da47b7f56d6e6e8fe905439505ba0121223221fa
Diego Molina <diegomolina83@gmail.com>
2021-07-08 16:28:54 +0200

    Http Request frtom API

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GifsPageComponent } from './gifs-page/gifs-page.component';
 import { BusquedaComponent } from './busqueda/busqueda.component';
+import { ResultadosComponent } from './resultados/resultados.component';
 
 
 
 @NgModule({
   declarations: [
     GifsPageComponent,
-    BusquedaComponent
+    BusquedaComponent,
+    ResultadosComponent
   ],
   imports: [
     CommonModule
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,16 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Datum, SearchGifsResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey: string = "AHBVHaLB83Jgk3ozTYATHI5UU2GQn1Cs";
   private _historial: string[] = [];
+
+  public resultados: Datum[] = [];
 
   get historial() {
     return [...this._historial];
   }
 
+
+  constructor(private http: HttpClient) {
+
+    this._historial = JSON.parse(localStorage.getItem("historial")!) || [];
+    // if (localStorage.getItem('historial')) {
+    //   this._historial = JSON.parse(localStorage.getItem("historial")!);
+    // }
+    this.resultados = JSON.parse(localStorage.getItem("resultados")!) || [];
+
+  }
   buscarGifs(query: string) {
 
     query = query.trim().toLowerCase();
@@ -21,6 +36,26 @@ export class GifsService {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
 
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+
     }
+
+
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}`)
+      .subscribe((resp) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
+
+      })
+
+
+
+
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=AHBVHaLB83Jgk3ozTYATHI5UU2GQn1Cs&q=albacete')
+    //   .then(resp => {
+    //     resp.json().then(data => console.log(data))
+    //   })
+
   }
 }
